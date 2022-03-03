6e01815c247d20ab725c9a36a8b28ba42b042ac5
LaSe28 <mattia.las28@gmail.com>
2022-03-03 19:30:39 +0100

    dont know

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ let max;
 let bombList = []
 let points = []
 let winPoints;
-
 let difficulty = document.querySelector('#difficulty');
 
 
@@ -20,7 +19,7 @@ btnPlay.addEventListener('click', function(){
     switch (difficulty.value){
         case 'hard':
             boxesNum = 49;
-            max = 49
+            max = 16
             boxesSize = 7
             winPoints = 33
             break;
@@ -43,13 +42,23 @@ btnPlay.addEventListener('click', function(){
         let square = document.createElement(`div`);
         square.classList.add('box')
         square.addEventListener('click', changeColor)
+        
+        for (let n = 0 ; n < 16; n++){
+            if(bombList[n]== m){
+                square.addEventListener('click', bombBoom)
+            }
+        }
+
+        
         square.style.height = `calc(100% / ${boxesSize})`
         square.style.width = `calc(100% / ${boxesSize})`
         square.innerHTML= m
         square.addEventListener('click', function(){
-            points.push(m)
+            if(!points.includes(m)){
+                points.push(m)
+            }
         })
-       
+
         grid.append(square)
         i = 0
         while (bombList.length < 16){
@@ -60,32 +69,34 @@ btnPlay.addEventListener('click', function(){
             i++
             console.log(bombList)
         }
-        for (n = 0 ; n < 16; n++){
-            if(bombList[n]== m){
-                    square.addEventListener('click', bombColor)
-            }
-        }
+        
         
     }   
     console.log(winPoints)
     console.log(points)
-});
+    
+
+});    
+
+let pointsAlert = document.querySelector('#points')
 function changeColor(){
     this.classList.add('active')
-}
-function bombColor(){
-    this.classList.add('bomb')
-}
-
+}    
+function bombBoom(){
+    this.classList.add('bomb-activate')
+    let lose = document.querySelector('#lose').classList.remove('hidden')
+    pointsAlert.classList.remove('hidden')
+    pointsAlert.innerHTML = `Hai fatto ${points.length} punti/o!`
+}    
 function generateRandomNum(min, max) {
     let randomNumber = Math.floor(Math.random()*(max - min + 1) +1 )
     return randomNumber
-}
+}    
 
 
-
-
-
+if(points.length==winPoints){
+        pointsAlert.innerHTML = `Hai vinto! Hai fatto ${points.length} punti/o!`
+    }
 
 
 
